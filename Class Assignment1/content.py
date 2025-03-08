443bed035e8a0332aa328d60eddcd749ffea2de7
sairarizvi6 <164424696+sairarizvi6@users.noreply.github.com>
2025-03-09 01:04:14 +0500

    Add files via upload

---
+++ Class Assignment1/content.py
@@ -0,0 +1,18 @@
+
+
+#✨🎯Assignment: Print the {Loop message} "Kharak Singh k kharakny se kharakti hain khirkiyan." (At least 5 times code run)
+
+#********Using a for loop**************
+
+print("using for loop:")
+for _ in range(5):
+  print("Kharak Singh k kharakny se kharakti hain khirkiyan")
+
+#***************Using while loop**************
+
+print("\n using while loop:")
+count = 0
+while count < 5:
+  print("Kharak Singh k kharakny se kharakti hain khirkiyan")
+  count +=1
+
